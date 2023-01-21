80100fdd5395b18956ec971edbd10f4dd9876ec2
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2023-01-21 11:36:30 +0530

    REPL

--- impls/python/step0_repl.py
+++ impls/python/step0_repl.py
@@ -1,29 +1,18 @@
-import sys, traceback
-import mal_readline
+def read() -> str:
+    return input()
 
-# read
-def READ(str):
-    return str
+def eval(character: str) -> str:
+    return character
 
-# eval
-def EVAL(ast, env):
-        #print("EVAL %s" % printer._pr_str(ast))
-        return ast
+def print_call(character: str):
+    print(character)
 
-# print
-def PRINT(exp):
-    return exp
 
-# repl
-def REP(str):
-    return PRINT(EVAL(READ(str), {}))
+def repl():
+        character = read()
+        character = eval(character)
+        print_call(character=character)
 
-# repl loop
 while True:
-    try:
-        line = mal_readline.readline("user> ")
-        if line == None: break
-        if line == "": continue
-        print(REP(line))
-    except Exception as e:
-        print("".join(traceback.format_exception(*sys.exc_info())))
+    print("user> ",end="")
+    repl()
